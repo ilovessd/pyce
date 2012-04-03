9ae9c7e5fac0f3f874a8a550a4f3b79b686e98f7
tioover <tioover@gmail.com>
2012-04-04 00:02:56 +0800

    setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python2
+# coding=utf-8
+from distutils.core import setup
+
+setup(name='PyCE',
+      version='0.2',
+      description='Python CIL Dict',
+      author='tioover',
+      author_email='tioover@gmail.com',
+      url='http://github.com/tioover/pyce/',
+      scripts=['pyce.py']
+     )
